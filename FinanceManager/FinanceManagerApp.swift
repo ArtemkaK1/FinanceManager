ab235b26579d38bfe77e146671cd914a1215be92
Artemiy Kirillov <artemkakirillov04@gmail.com>
2024-07-20 11:17:53 +0300

    Initial Commit

---
+++ FinanceManager/FinanceManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceManagerApp.swift
+//  FinanceManager
+//
+//  Created by Artemiy Kirillov on 20.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct FinanceManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
